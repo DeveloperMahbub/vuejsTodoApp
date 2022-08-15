49861a8e26dbea196685aea9f3adc4a7c3a77e5c
Mahbubur Rahman <mahbubur2310@gmail.com>
2022-08-15 14:09:58 +0600

    Project Setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template lang="">
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      message: "It is Work",
+    };
+  },
+};
+</script>
+<style lang=""></style>
