560f96c1795496dffb7cd1508a46449a64bd50ea
Mahbubur Rahman <mahbubur2310@gmail.com>
2022-08-15 17:06:09 +0600

    Todo App Completed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,11 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
-</script>
-
-<template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
-    </h3>
+<template lang="">
+  <div>
+    <h1>Component 1</h1>
   </div>
 </template>
-
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
+<script>
+export default {
+  name: "HellowWorld",
+};
+</script>
+<style lang=""></style>
